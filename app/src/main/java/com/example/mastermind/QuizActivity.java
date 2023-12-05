f586e7dd49a1c8b07686186072de0b128c545c59
ismael <ismaelcontreras4377@gmail.com>
2023-12-04 23:10:17 -0700

    Quiz mode now flips cards and can shift through cards. Prev and Next button work. Clicking on card flips it.

--- app/src/main/java/com/example/mastermind/QuizActivity.java
+++ app/src/main/java/com/example/mastermind/QuizActivity.java
@@ -16,7 +16,7 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
     private Folder folder;
 
     private ArrayList<FlashCard> cards;
-    private int cardPos = 0;
+    private int cardIt = 0;
 
     private FlashCard currCard;
     @Override
@@ -26,9 +26,47 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(binding.getRoot());
         folder = (Folder) getIntent().getSerializableExtra("Folder");
         cards = folder.getFlashCards();
-        currCard = cards.get(cardPos);
+        currCard = cards.get(cardIt);
         binding.card.setText(currCard.getQuestion());
         binding.titleQuiz.setText(folder.getName());
+
+        binding.card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code for the quiz button goes here
+                if(binding.card.getText() == currCard.getAnswer()){
+                    binding.card.setText(currCard.getQuestion());
+
+                }
+                else{
+                    binding.card.setText(currCard.getAnswer());
+                }
+            }
+        });
+
+        binding.nextCardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code for the quiz button goes here
+                if(cardIt < folder.getFlashCards().size() - 1){
+                    cardIt += 1;
+                    currCard = cards.get(cardIt);
+                    binding.card.setText(currCard.getQuestion());
+                }
+            }
+        });
+
+        binding.prevCardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code for the quiz button goes here
+                if(cardIt > 0){
+                    cardIt -= 1;
+                    currCard = cards.get(cardIt);
+                    binding.card.setText(currCard.getQuestion());
+                }
+            }
+        });
     }
 
     @Override
